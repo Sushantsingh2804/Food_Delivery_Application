cb11df6f8e9b1593511077c76659a9c21dd80837
AKASH2082001 <akashbme208@gmail.com>
2022-04-12 17:46:34 -0700

    Transaction

--- Restaurant.py
+++ Restaurant.py
@@ -1,25 +1,3 @@
-from flask import Flask, request, render_template, flash
-import sqlite3 as s
-
-from werkzeug.utils import redirect
-
-connection = s.connect("foodex.db", check_same_thread=False)
-table1 = connection.execute("SELECT NAME FROM sqlite_master WHERE type='table' AND name= 'RESTAURANT'").fetchall()
-
-if table1 != []:
-    print("Table Already Exist")
-else:
-    connection.execute('''CREATE TABLE RESTAURANT(
-                        RESTAURANT_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                        NAME_OF_RESTAURANT TEXT,
-                        RESTAURANT_PASSWORD TEXT,
-                        ADDRESS TEXT,
-                        MOBILE_NO INTEGER,
-                        RESTAURANT_WALLET_BALANCE INTEGER
-                       )''')
-
-    print("Table Created Successfully")
-
 restaurant=Flask(__name__)
 
 @restaurant.route("/",methods = ["GET","POST"])
